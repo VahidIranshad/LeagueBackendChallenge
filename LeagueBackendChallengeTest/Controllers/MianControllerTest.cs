962d1e36231830ed9d7e9c83fffe3e077f024fee
vahidiranshad <vahid.iranshad@gmail.com>
2023-05-07 15:33:35 +0430

    edit references

--- LeagueBackendChallengeTest/Controllers/MianControllerTest.cs
+++ LeagueBackendChallengeTest/Controllers/MianControllerTest.cs
@@ -1,8 +1,5 @@
 ﻿using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Text;
-using Xunit.Sdk;
 
 namespace LeagueBackendChallengeTest.Controllers
 {
